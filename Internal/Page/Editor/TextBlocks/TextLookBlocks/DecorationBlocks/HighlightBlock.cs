9bc4dfabbc4904165f6b8a0a4ebf4ff6a7a339e9
prizrak9 <pavelgolotuk@gmail.com>
2019-03-08 16:46:05 +0200

    now every decoration is independent. Keep text size is < 5000.

--- Internal/Page/Editor/TextBlocks/TextLookBlocks/DecorationBlocks/HighlightBlock.cs
+++ Internal/Page/Editor/TextBlocks/TextLookBlocks/DecorationBlocks/HighlightBlock.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+using System.Linq;
 
 namespace ScriptEditor
 {
@@ -26,6 +27,71 @@ namespace ScriptEditor
         //    drawingContext.DrawRectangle(Brush, Pen, new Rect(left, top, width, height));
         //}
 
+        public void RenderRange(Editor editor, DrawingContext drawingContext, int row, int inRowPosition, int count)
+        {
+            double left = inRowPosition * editor.LetterWidth;
+            double top = row * editor.LetterHeight;
+
+
+            double right = (inRowPosition + count) * editor.LetterWidth;
+            double bottom = (row + 1) * editor.LetterHeight;
+
+
+            //var isSelection = Tags.Contains("selection");
+
+            //if (isSelection)
+            //{
+            //    var poss = editor.Document.GetPositionInText(editor.SelectionRange.left);
+            //    left = poss.inRowPosition * editor.LetterWidth;
+            //    poss = editor.Document.GetPositionInText(editor.SelectionRange.right + 1);
+            //    right = (poss.inRowPosition) * editor.LetterWidth;
+            //}
+
+
+            double width = right - left;
+            double height = bottom - top;
+
+            left = (int)left;
+            top = (int)top;
+            width = (int)width + 1;
+            height = (int)height + 1;
+
+            drawingContext.DrawRectangle(Brush, Pen, new Rect(left, top, width, height));
+        }
+
+
+        public void RenderRange(Editor editor, DrawingContext drawingContext, string str, (int inStringPosition, int row, int inRowPosition) pos)
+        {
+            double left = pos.inRowPosition * editor.LetterWidth;
+            double top = pos.row * editor.LetterHeight;
+
+
+            double right = (pos.inRowPosition + str.Length) * editor.LetterWidth;
+            double bottom = (pos.row + 1) * editor.LetterHeight;
+
+
+            var isSelection = Tags.Contains(Tag.Selection);
+
+            if (isSelection)
+            {
+                var poss = editor.Document.GetPositionInText(editor.SelectionRange.left);
+                left = poss.inRowPosition * editor.LetterWidth;
+                poss = editor.Document.GetPositionInText(editor.SelectionRange.right + 1);
+                right = (poss.inRowPosition) * editor.LetterWidth;
+            }
+
+
+            double width = right - left;
+            double height = bottom - top;
+
+            left = (int)left;
+            top = (int)top;
+            width = (int)width + 1;
+            height = (int)height + 1;
+
+            drawingContext.DrawRectangle(Brush, Pen, new Rect(left, top, width, height));
+        }
+
         public void OnRender(Editor editor, DrawingContext drawingContext, int inStringPosition)
         {
             var startInfo = editor.Document.GetPositionInText(inStringPosition);
@@ -36,13 +102,23 @@ namespace ScriptEditor
             double right = (startInfo.inRowPosition + 1) * editor.LetterWidth;
             double bottom = (startInfo.row + 1) * editor.LetterHeight;
 
+            var isSelection = Tags.Contains(Tag.Selection);
+
+            if (isSelection)
+            {
+                var poss = editor.Document.GetPositionInText(editor.SelectionRange.left);
+                left = poss.inRowPosition * editor.LetterWidth;
+                poss = editor.Document.GetPositionInText(editor.SelectionRange.right + 1);
+                right = (poss.inRowPosition) * editor.LetterWidth;
+            }
+
             double width = right - left;
             double height = bottom - top;
 
             left = (int)left;
             top = (int)top;
-            width = (int)width+1;
-            height = (int)height+1;
+            width = (int)width + 1;
+            height = (int)height + 1;
 
             drawingContext.DrawRectangle(Brush, Pen, new Rect(left, top, width, height));
         }
